57e04cdaf0b2f7d9385976d35a37623cec723962
yugang <yugang@chehang168.com>
2021-07-23 19:48:25 +0800

    ant design vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue';
 import App from './App.vue'
+
+import 'ant-design-vue/dist/antd.css';
+Vue.config.productionTip = false;
+
+Vue.use(Antd);
 
 Vue.config.productionTip = false
 
